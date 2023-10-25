7ddfc60e03d22f3524f6ddc34b04217f343a4985
GonzaloSantizo <san21504@uvg.edu.gt>
2023-10-25 09:09:40 -0600

    first commit

---
+++ gl.py
@@ -0,0 +1,17 @@
+import glm
+from OpenGL.GL import *
+
+
+class Renderer(object):
+    def __init__(self, screen):
+        self.screen = screen
+        _, _, self.width, self.height = screen.get_rect()
+
+        self.clearColor = [0,0,0]
+
+        glEnable(GL_DEPTH_TEST)
+        glViewport(0,0, self.width, self.height)
+
+    def render(self):
+        glClearColor(self.clearColor[0], self.clearColor[1], self.clearColor[2], 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
